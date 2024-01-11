2ebd92126e968782b854281901b7f3940ab06c76
Bassanelli Simone <bassanelli.simone.studente@itispaleocapa.it>
2024-01-11 10:29:20 +0100

    Aggiungere i file di progetto.

---
+++ 14 - Esercitazione test con composite/Form1.cs
@@ -0,0 +1,10 @@
+namespace _14___Esercitazione_test_con_composite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
