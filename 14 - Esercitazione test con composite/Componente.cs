b90b483da55cfd1fbf2e0a5582253771664e1f06
Bassanelli Simone <bassanelli.simone.studente@itispaleocapa.it>
2024-01-23 12:45:56 +0100

    Fatta la classe componente e domanda

---
+++ 14 - Esercitazione test con composite/Componente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14___Esercitazione_test_con_composite
+{
+    public interface IComponent
+    {
+        void Aggiunta(IComponent comp);
+        
+        void Rimuovi(int index);
+        
+        IComponent GetChild(int index);
+        
+        bool Equals(object obj);
+        
+        string ToString();
+        
+        double Punteggio();
+    }
+}
+
